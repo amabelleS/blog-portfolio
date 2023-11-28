badf655585ea2279e98c24d7918e9bf3ca014a53
amabelleS <amabelletr@gmail.com>
2023-11-29 00:02:10 +0200

    about text

--- app/components/home-page/About.tsx
+++ app/components/home-page/About.tsx
@@ -2,13 +2,16 @@
 import React from 'react'
 import { motion } from "framer-motion"
 
-const ABOUT_TEXT = " I'm a dedicated full-stack developer specializing in React, Node.js, Next.js, and various other technologies. Currently, I'm fortunate to be working in a small startup where my passion for problem-solving and knack for debugging are put to the test. From crafting seamless user experiences to implementing clean and efficient code, I strive to deliver exceptional end-to-end solutions. With a genuine love for what I do and a continuous drive for learning, I'm excited to contribute my skills to create meaningful and innovative projects."
+const hasadnaLink = 'https://open-bus-map-search.hasadna.org.il/dashboard'
+const hamechinaGithub = 'https://github.com/Bootcamp-Projects-Hamechina/hamechina'
+
+const ABOUT_TEXT = "I'm a dedicated full-stack developer with a focus on React, Node.js, and my favorite, Next.js (any questions about it? Just ask!). Currently, I'm fortunate to be deeply involved in several impactful projects: I'm an active contributor to open-source initiatives like open-bus-search and the initiator and maintainer of the Hamechina project, alongside ongoing work on Hasbara projects. In the past year, I honed my skills in a small startup where my passion for problem-solving and knack for debugging were put to the test. From crafting seamless user experiences to implementing clean and efficient code, I consistently strive to deliver exceptional end-to-end solutions. Driven by a genuine love for my work and an insatiable thirst for learning, I'm thrilled about the opportunity to contribute my skills to create meaningful and innovative projects."
 
 function About() {
   return (
     <section className='mb-12'>
         <div className="mx-auto max-w-2xl">
-        <p className=" font-semibold sm:text-2xl md:text-3xl text-center px-8 lg:px-0 text-teal-100"
+        <p className=" font-semibold sm:text-2xl md:text-3xl text-center px-4 lg:px-0 text-teal-100"
         >
             {/* Animate the about text */}
            {ABOUT_TEXT.split('').map((char, index) => {
