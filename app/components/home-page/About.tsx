214c7974d0aca73a528f78addbc86aa2c21e5dae
amabelleS <amabelletr@gmail.com>
2023-12-11 02:57:40 +0200

    updated about & exp.

--- app/components/home-page/About.tsx
+++ app/components/home-page/About.tsx
@@ -6,13 +6,14 @@ import Link from 'next/link'
 const hasadnaLink = 'https://open-bus-map-search.hasadna.org.il/dashboard'
 const hamechinaGithub = 'https://github.com/Bootcamp-Projects-Hamechina/hamechina'
 
-const ABOUT_TEXT = "I'm a dedicated full-stack developer with a focus on React, Node.js, and my favorite, Next.js (any questions about it? Just ask!). Currently, I'm fortunate to be deeply involved in several impactful projects: I'm an active contributor to open-source initiatives like open-bus-search and the initiator and maintainer of the Hamechina project, alongside ongoing work on Hasbara projects. In the past year, I honed my skills in a small startup where my passion for problem-solving and knack for debugging were put to the test. From crafting seamless user experiences to implementing clean and efficient code, I consistently strive to deliver exceptional end-to-end solutions. Driven by a genuine love for my work and an insatiable thirst for learning, I'm thrilled about the opportunity to contribute my skills to create meaningful and innovative projects."
+const ABOUT_TEXT = "I'm a dedicated full-stack developer with a focus on React, Node.js, and my favorite, Next.js (any questions about it? Just ask!). Currently, I'm fortunate to be deeply involved in several impactful projects: Open-source initiatives like open-bus-search, Hamechina project, alongside ongoing work on Hasbara projects. In the past year, I honed my skills in a small startup where my passion for problem-solving and knack for debugging were put to the test. From crafting seamless user experiences to implementing clean and efficient code, I consistently strive to deliver exceptional end-to-end solutions. Driven by a genuine love for my work and an insatiable thirst for learning, I'm thrilled about the opportunity to contribute my skills to create meaningful and innovative projects."
 
 function About() {
   return (
     <section className='mb-12'>
         <div className="mx-auto max-w-2xl text-center">
-        <p className="font-semibold sm:text-2xl md:text-3xl px-4 lg:px-0 text-teal-100 border-b-2 border-teal-100 pb-4"
+        <p className="font-semibold sm:text-2xl md:text-3xl px-4 lg:px-0 text-teal-100 pb-4"
+        // <p className="font-semibold sm:text-2xl md:text-3xl px-4 lg:px-0 text-teal-100 border-b-2 border-teal-100 pb-4"
         >
             {/* Animate the about text */}
            {ABOUT_TEXT.split('').map((char, index) => {
@@ -28,16 +29,6 @@ function About() {
                 )
            })}
         </p>
-        <Link 
-          href={hasadnaLink}
-          className=" font-semibold sm:text-2xl md:text-3xl text-center px-4 lg:px-0 text-teal-300"
-            >open-bus website
-        </Link>
-        <Link 
-          href={hamechinaGithub}
-          className=" font-semibold sm:text-2xl md:text-3xl text-center px-4 lg:px-0 text-teal-300"
-            >Hmechina GitHub repo
-        </Link>
         {/* <motion.p className="mt-12 mb-12 text-3xl text-center dark:text-white/90">
            Hello, I'm [Your Name], a dedicated full-stack developer specializing in React, Node.js, Next.js, and various other technologies. Currently, I'm fortunate to be working in a small startup where my passion for problem-solving and knack for debugging are put to the test. From crafting seamless user experiences to implementing clean and efficient code, I strive to deliver exceptional end-to-end solutions. With a genuine love for what I do and a continuous drive for learning, I'm excited to contribute my skills to create meaningful and innovative projects.
         </motion.p> */}
--- data/expirience.ts
+++ data/expirience.ts
@@ -1,13 +1,23 @@
 const experience = [
     {
         position: "Full-Stack Developer",
+        dates: "7/10/2023 - Present",
+        company: "Open Source Projects",
+        company_url: "https://open-bus-map-search.hasadna.org.il/dashboard",
+        description: "I actively contribute to open-source and Israely Hasbara projects. 'Hamechina' a Next.js app tailored for a community of developers, a project I initiated and currently maintain. In the 'Maakaf' community, mainly the open-bus-search. Hasbara projects - The Antisemetic Reporter - in development",   
+        location: "Tel-Aviv, Israel",
+        key_techs: ["Next.js", "Node.js", "TailwindCSS", "TypeScript", "Git", "GitHub", "Docker", "Figma"],
+        id: 1,
+    },
+    {
+        position: "Full-Stack Developer",
         dates: "1/2023 - Present",
         company: "Taytan-Tech",
         company_url: "/",
         description: "As a full-stack developer at a startup, I developing a dynamic web application using React.js, Typescript, Node.js, and MongoDB. My responsibilities included developing both the front-end and back-end of the application. Among other things, I implemented a user-permissions system, and developed tour registration functionality. Collaborating with my team, gathering requirements and ensuring stability and performance through testing and debugging.",   
         location: "Tel-Aviv, Israel",
         key_techs: ["React", "Node.js", "Matirial-Ui", "MongoDB", "Express.js", "TypeScript", "HTML", "CSS", "Git", "GitHub", "Docker", "AWS", "Figma"],
-        id: 1,
+        id: 2,
     },
     {
         position: "Full-Stack Developer",
@@ -17,7 +27,7 @@ const experience = [
         description: "In collaboration with a team, I developed an app for Psagot, a non-profit organization focused on providing excellent education to teens in the periphery. The tech stack used included React.js (along with Redux toolkit) on the client side, node.js and express.js on the server side, and MySql for the database. I developed the graduates section of the app.",   
         location: "Tel-Aviv, Israel",
         key_techs: ["React", "firebase", "Framer Motion"],
-        id: 2,
+        id: 3,
     },
 ]
 
