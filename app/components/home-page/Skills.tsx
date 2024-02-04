1e755f688ceaff7e935ff28096c2920c92ca7189
Amabelle Trachtenberg <amabelletr@gmail.com>
2024-02-04 22:42:15 +0200

    CSS changes the size of image => include the styles 'width: "auto"' or 'height: "auto"'

--- app/components/home-page/Skills.tsx
+++ app/components/home-page/Skills.tsx
@@ -14,37 +14,48 @@ export default function Skills({}: Props) {
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg"
-                    width={121}
-                    height={111}
                     alt="mongodb logo"
-                    className='hover:motion-safe:animate-pulse'
+                    width="0"
+                    height="0"
+                    style={{ width: '121px', height: 'auto' }}
+                    className="hover:motion-safe:animate-pulse"
+                    // className='hover:motion-safe:animate-pulse w-[121] h-[111]'
                 />
             </div>
             
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg"
-                    width={88}
-                    height={77}
-                    className='hover:motion-safe:animate-pulse'
+                    // width={88}
+                    // height={77}
+                    width="0"
+                    height="0"
+                    className="hover:motion-safe:animate-pulse"
+                    style={{ width: '88px', height: 'auto' }}
                     alt="nodejs logo"
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg"
-                    width={88}
-                    height={77}
+                    // width={88}
+                    // height={77}
                     alt="Naxtjs logo"
-                    className='hover:motion-safe:animate-pulse'
+                    width="0"
+                    height="0"
+                    style={{ width: '88px', height: 'auto' }}
+                    className="hover:motion-safe:animate-pulse"
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-                    width={42}
-                    height={32}
-                    className='hover:motion-safe:animate-pulse font-bold'
+                    // width="42"
+                    // height="32"
+                    width="0"
+                    height="0"
+                    style={{ width: '42px', height: 'auto' }}
+                    className="hover:motion-safe:animate-pulse"
                     alt="React logo"
                 />
             </div>
@@ -52,54 +63,60 @@ export default function Skills({}: Props) {
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
-                    width={32}
-                    height={23}
+                    width="0"
+                    height="0"
                     alt="JavaScript logo"
+                    style={{ width: '32px', height: 'auto' }}
                     className='hover:motion-safe:animate-pulse'
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
-                    width={32}
-                    height={23}
+                    width="0"
+                    height="0"
                     alt="TypeScript logo"
+                    style={{ width: '32px', height: 'auto' }}
                     className='hover:motion-safe:animate-pulse'
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
-                    width={38}
-                    height={27}
+                    width="0"
+                    height="0"
                     alt="HTML logo"
+                    style={{ width: '38px', height: 'auto' }}
                     className='hover:motion-safe:animate-pulse'
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg"
-                    width={32}
-                    height={23}
+                    width="0"
+                    height="0"
                     alt="CSS logo"
+                    style={{ width: '32px', height: 'auto' }}
                     className='hover:motion-safe:animate-pulse'
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg"
-                    width={32}
-                    height={23}
+                    width="0"
+                    height="0"
                     alt="Vue.js logo"
+                    style={{ width: '32px', height: 'auto' }}
                     className='hover:motion-safe:animate-pulse'
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Font_Awesome_5_brands_angular.svg"
-                    width={32}
-                    height={23}
+                    width="0"
+                    height="0"
                     alt="Angular logo"
+                    style={{ width: '32px', height: 'auto' }}
                     className='hover:motion-safe:animate-pulse'
                 />
             </div>
@@ -109,23 +126,26 @@ export default function Skills({}: Props) {
                     width={66}
                     height={55}
                     alt="SQL logo"
+                    // style={{ width: '32px', height: 'auto' }}
                     className='hover:motion-safe:animate-pulse'
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Docker_%28container_engine%29_logo.svg"
-                    width={121}
-                    height={111}
+                    width="0"
+                    height="0"
                     alt="Docker logo"
+                    style={{ width: '121px', height: 'auto' }}
                     className='hover:motion-safe:animate-pulse'
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg"
-                    width={66}
-                    height={55}
+                    width="66"
+                    height="0"
+                    style={{ width: '66x', height: 'auto' }}
                     alt="Git logo"
                     className='hover:motion-safe:animate-pulse'
                 />
@@ -136,14 +156,16 @@ export default function Skills({}: Props) {
                     width={32}
                     height={32}
                     alt="GitHub logo"
+                    // style={{ width: '32px', height: 'auto' }}
                     className='hover:motion-safe:animate-pulse'
                 />
             </div>
             <div className='p-4 mx-auto col-span-1'>
                 <Image
                     src="https://upload.wikimedia.org/wikipedia/commons/3/37/Firebase_Logo.svg"
-                    width={111}
-                    height={101}
+                    width="0"
+                    height="0"
+                    style={{ width: '111px', height: 'auto' }}
                     alt="Firebase logo"
                     className='hover:motion-safe:animate-pulse'
                 />
--- app/components/layout/Logo.tsx
+++ app/components/layout/Logo.tsx
@@ -7,10 +7,11 @@ function Logo() {
     <Image
     src={logo}
     alt="Amabelle's logo"
-    width={142}
-    height={22}
+    width="0"
+    height="0"
     className="bg-mint text-mint fill-current m-0"
-    priority={true}
+    // priority={true}
+    style={{ width: '142px', height: 'auto' }}
     // style={{objectFit: "contain"}}
   />
   </div>
